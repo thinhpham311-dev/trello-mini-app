c963f61800385e7ddd5d205ae3605a39d3865ff9
thinhpham311-dev <phamthinh30522@gmail.com>
2026-02-04 12:54:24 +0700

    connect to store firestore database

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+require('dotenv').config();
 const path = require("path");
 const cors = require("cors");
+const { db } = require('./config/firebase');
 
 const indexRouter = require("./routes/index");
 
@@ -9,6 +11,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.get('/test-db', async (req, res) => {
+  await db.collection('users').add({
+    name: 'Thinh',
+    createdAt: new Date(),
+  });
+
+  res.send('Firebase connected ✅');
+});
 
 app.use("/", indexRouter);
 
