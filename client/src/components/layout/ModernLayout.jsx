928948faf8364b0a26ac2c38c4ddfdd20f495b04
thinhpham311-dev <phamthinh30522@gmail.com>
2026-02-05 16:22:53 +0700

    update header and view

--- client/src/components/layout/ModernLayout.jsx
+++ client/src/components/layout/ModernLayout.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
+import Header from '../template/Header'
+import View from '../../views'
 
+const HeaderActionsStart = () => {
+	return (
+		<>
+			header action start
+		</>
+	)
+}
 
+const HeaderActionsEnd = () => {
+	return (
+		<>
+			header action end
+		</>
+	)
+}
 
-const ModernLayout = () => {
+const ModernLayout = props => {
 	return (
 		<div className="app-layout-modern flex flex-auto flex-col">
 			<div className="flex flex-auto min-w-0">
-				abc
+				<div className="flex flex-col flex-auto min-h-screen min-w-0 relative w-full bg-white dark:bg-gray-800 border-l border-gray-200 dark:border-gray-700">
+					<Header 
+						className="border-b border-gray-200 dark:border-gray-700" 
+						headerEnd={<HeaderActionsEnd />} 
+						headerStart={<HeaderActionsStart />}
+					/>
+					<View {...props} />
+				</div>
 			</div>
 		</div>
 	)
